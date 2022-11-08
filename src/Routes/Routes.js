e5ec2513650c964be9c7aab1b5041184bc858ec6
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-11-08 17:54:50 +0600

    authentication implement

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,6 +4,8 @@ import Main from "../layout/Main";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Services from "../Pages/Services/Services";
 import ServiceDetails from "../Pages/Services/ServiceDetails";
+import Login from "../Pages/Login/Login/Login";
+import Register from "../Pages/Login/Register/Register";
 
 export const router = createBrowserRouter([
     {
@@ -23,6 +25,14 @@ export const router = createBrowserRouter([
                 path: '/services/:id',
                 element: <ServiceDetails></ServiceDetails>,
                 loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     }
