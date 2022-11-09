d836ff5d849a183ab884d3620ad5f3318a444747
Md Mehedi Ahsan <ahsanmehedi041@gmail.com>
2022-11-09 10:52:14 +0600

    reviews deleted in db

--- src/Pages/Reviews/MyReviews.js
+++ src/Pages/Reviews/MyReviews.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider';
 import ReviewRow from './ReviewRow';
+import Swal from 'sweetalert2';
 
 const MyReviews = () => {
     const {user} = useContext(AuthContext);
@@ -12,24 +13,67 @@ const MyReviews = () => {
         .then(data => setReviews(data))
     }, [user?.email])
 
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/reviews/${id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            );
+                            const remaining = reviews.filter(rvw => rvw._id !== id);
+                            setReviews(remaining);
+                        }
+                    });
+            }
+        });
+    }
+
     return (
         <div className='container mx-auto'>
-            <h1 className='text-3xl font-bold text-center'>You have {reviews.length} reviews</h1>
-            <table class="min-w-full border-collapse block md:table">
-                <thead class="block md:table-header-group">
-                    <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
-                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Service Name</th>
-                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">User Name</th>
-                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Review</th>
-                        <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
-                    </tr>
-                </thead>
-                <tbody class="block md:table-row-group">
-                    {
-                        reviews.map(review => <ReviewRow key={review._id} review={review}></ReviewRow>)
-                    }		
-                </tbody>
-            </table>
+            {
+                reviews.length !== 0 ?
+                <>
+                <h1 className='text-3xl font-semibold text-center my-10'>You have {reviews.length} reviews</h1>
+                <table class="min-w-full border-collapse block md:table">
+                    <thead class="block md:table-header-group">
+                        <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                            <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Service Name</th>
+                            <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">User Name</th>
+                            <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Review</th>
+                            <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="block md:table-row-group">
+                        {
+                            reviews.map(review => <ReviewRow
+                            key={review._id} 
+                            review={review}
+                            handleDelete={handleDelete}
+                            ></ReviewRow>)
+                        }		
+                    </tbody>
+                </table>
+                </> 
+                :
+                <div>
+                    <h1 className='text-3xl font-semibold text-center my-20'><h1 className='text-3xl font-semibold text-center my-10'>No reviews were added</h1></h1>
+                </div>
+            }
         </div>
     );
 };
--- src/Pages/Reviews/ReviewRow.js
+++ src/Pages/Reviews/ReviewRow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const ReviewRow = ({review}) => {
-    const {serviceName, reviewer, message} = review;
+const ReviewRow = ({review, handleDelete}) => {
+    const {_id, serviceName, reviewer, message} = review;
     return (
         <tr class="bg-white border border-grey-500 md:border-none block md:table-row">
             <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Services</span>{serviceName}</td>
@@ -9,8 +9,8 @@ const ReviewRow = ({review}) => {
             <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Review</span>{message}</td>
             <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                 <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
-                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 border-blue-500 rounded mr-2">Edit</button>
+                <button onClick={ () => handleDelete(_id) } class="bg-red-500 hover:bg-red-600 text-white font-semibold py-1 px-2 border-red-500 rounded">Delete</button>
             </td>
         </tr>
     );
